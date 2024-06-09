d35305a1d1dd36916db89db158830c1d761d5f47
denjayx <denyw602@gmail.com>
2024-06-09 20:01:42 +0700

    feat: packages page

--- client/src/scripts/routes/router.jsx
+++ client/src/scripts/routes/router.jsx
@@ -2,10 +2,13 @@ import {
   createBrowserRouter,
   Route,
   createRoutesFromElements,
+  Navigate,
 } from 'react-router-dom'
 import RootLayout from '../views/layouts/RootLayout'
 import Homepage from '../views/pages/Homepage.jsx'
 import Root from '../views/layouts/Root'
+import Dashboard from '../views/layouts/Dashboard.jsx'
+import Packages from '../views/pages/Dashboard/Packages.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -13,6 +16,10 @@ const router = createBrowserRouter(
       <Route element={<Root />}>
         <Route path="/" element={<RootLayout />}>
           <Route index element={<Homepage />}></Route>
+          <Route path="dashboard" element={<Dashboard />}>
+            <Route index element={<Navigate to={'packages'} />} />
+            <Route path="packages" element={<Packages />}></Route>
+          </Route>
         </Route>
       </Route>
     </>,
