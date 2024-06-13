d2845833bd53be5f2659882126c6f017428d13dd
denjayx <denyw602@gmail.com>
2024-06-13 22:12:06 +0700

    feat: history page

--- client/src/scripts/routes/router.jsx
+++ client/src/scripts/routes/router.jsx
@@ -5,10 +5,21 @@ import {
   Navigate,
 } from 'react-router-dom'
 import RootLayout from '../views/layouts/RootLayout'
-import Homepage from '../views/pages/Homepage.jsx'
+import TourPackage from '../views/pages/TourPackage'
+import PackageHistory from '../views/pages/HistoryPage'
+import HistoryDetails from '../views/pages/HistoryDetails'
+import Login from '../views/pages/Login'
+import Register from '../views/pages/Register'
+import Homepage from '../views/pages/Homepage'
+import Auth from '../views/layouts/Auth'
+import Detail from '../views/pages/Detail'
+import Dashboard from '../views/layouts/Dashboard'
+import Packages from '../views/pages/Dashboard/Packages'
+import Orders from '../views/pages/Dashboard/Orders'
+import PackagesDetail from '../views/pages/Dashboard/PackagesDetail'
+import AddPackage from '../views/pages/Dashboard/AddPackage'
 import Root from '../views/layouts/Root'
-import Dashboard from '../views/layouts/Dashboard.jsx'
-import Packages from '../views/pages/Dashboard/Packages.jsx'
+import Profile from '../views/pages/Profile'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -16,10 +27,22 @@ const router = createBrowserRouter(
       <Route element={<Root />}>
         <Route path="/" element={<RootLayout />}>
           <Route index element={<Homepage />}></Route>
+          <Route path="packages" element={<TourPackage />}></Route>
+          <Route path="packages/:id" element={<Detail />}></Route>
+          <Route path="history" element={<PackageHistory />}></Route>
+          <Route path="history/:id" element={<HistoryDetails />}></Route>
           <Route path="dashboard" element={<Dashboard />}>
             <Route index element={<Navigate to={'packages'} />} />
             <Route path="packages" element={<Packages />}></Route>
+            <Route path="packages/add" element={<AddPackage />}></Route>
+            <Route path="packages/:id" element={<PackagesDetail />}></Route>
+            <Route path="orders" element={<Orders />}></Route>
           </Route>
+          <Route path="/profile" element={<Profile />}></Route>
+        </Route>
+        <Route element={<Auth />}>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
         </Route>
       </Route>
     </>,
