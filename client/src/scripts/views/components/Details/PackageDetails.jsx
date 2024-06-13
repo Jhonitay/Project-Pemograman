4da8c9e77435250db8c1741ab6711212c570b95d
denjayx <denyw602@gmail.com>
2024-06-13 22:14:20 +0700

    feat: input image component

--- client/src/scripts/views/components/Details/PackageDetails.jsx
+++ client/src/scripts/views/components/Details/PackageDetails.jsx
@@ -1,90 +1,81 @@
-/* eslint-disable react/prop-types */
-import CardImage from '../../../../assets/images/card-image.png'
-import Avatar from '../../../../assets/avatar.png'
-import OrderForm from './OrderForm'
-import { useState } from 'react'
-import CardPackage from './CardPackage'
+import { useEffect, useState } from 'react'
+import { NavLink, useOutletContext, useParams } from 'react-router-dom'
+import { BASE_IMAGEURL, getProfilePackageDetail } from '../../../data/api'
+import DestinationDetails from '../../components/Details/DestinationDetails'
+import Button from '../../components/Buttons/Button'
 
-const PackageDetails = () => {
-  return (
-    <section
-      id="packagedetails"
-      className=" flex w-full flex-col items-center gap-6 rounded-xl lg:flex-row"
-    >
-      <div className="h-full w-full overflow-hidden rounded-lg md:rounded-l-lg lg:w-6/12">
-        <img
-          src={CardImage}
-          alt="Thumbnail Card"
-          className=" W-full h-full object-cover "
-        />
-      </div>
-      <CardPackage />
+const PackagesDetail = () => {
+  const { id } = useParams()
+  const { user } = useOutletContext()
+  const [packageDetail, setPackageDetail] = useState({
+    id: '',
+    packageName: '',
+    thumbnailUrl: '',
+    price: 0,
+    description: '',
+    serviceDuration: 0,
+    destinations: [],
+  })
 
-      <div className="w-full lg:w-6/12 ">
-        <OrderForm />
-      </div>
-
-const PackageDetails = ({ showImage = true }) => {
-  const [showOrderForm, setShowOrderForm] = useState(false)
-
-  const handlePesanSekarang = () => {
-    setShowOrderForm(true)
+  const formatCurrency = (amount) => {
+    return new Intl.NumberFormat('id-ID', {
+      style: 'currency',
+      currency: 'IDR',
+      minimumFractionDigits: 0,
+    }).format(amount)
   }
 
+  useEffect(() => {
+    const fetchPackageDetail = async () => {
+      const packageDetail = await getProfilePackageDetail(user.token, id)
+      setPackageDetail(packageDetail)
+    }
+
+    fetchPackageDetail()
+  }, [id, user])
+
   return (
-    <section
-      id="packagedetails"
-      className=" flex w-full flex-col items-center gap-6 rounded-xl lg:flex-row "
-    >
-      {showImage && (
+    <div className="flex flex-col p-10">
+      <section
+        id="packagedetails"
+        className=" flex w-full flex-col items-center gap-6 rounded-xl lg:flex-row "
+      >
         <div className="h-full w-full overflow-hidden rounded-lg md:rounded-l-lg lg:w-5/12">
           <img
-            src={CardImage}
+            src={BASE_IMAGEURL + packageDetail.thumbnailUrl}
             alt="Thumbnail Card"
             className=" h-full w-full object-cover "
           />
         </div>
-      )}
-      <section className="flex h-full w-full flex-col justify-between gap-1 rounded-lg border-gray-200 bg-white p-5 md:rounded-l-lg">
-        <div className="flex items-center">
-          <img
-            src={Avatar}
-            width="32px"
-            alt="tourguide avatar"
-            className="avatar"
-          />
-          <span>Abram Saris</span>
-        </div>
-        <h5 className="mb-2 text-xl font-bold tracking-tight text-primary-950">
-          Pura Tanah Lot - Pura Uluwatu - Pantai Kuta
-        </h5>
-        <p className="mb-3 font-normal text-gray-700 ">
-          Lorem ipsum dolor sit amet consectetur. Convallis montes nulla cras
-          in. Ut viverra odio et pharetra dictum nunc ultrices. Mattis lorem
-          amet egestas blandit ut. Sed hendrerit convallis scelerisque arcu
-          pulvinar id at.
-        </p>
-        <p className="mb-3 font-normal text-primary-950">
-          <span className="font-bold">3 Hari</span> Layanan
-        </p>
-        <section className="mb-4 flex flex-col items-center justify-between gap-2  md:flex-row">
-          <h4 className="text-xl font-bold text-primary-500">Rp500.000,-</h4>
-          {!showOrderForm && (
-            <div className="relative">
-              <button
-                type="button"
-                className="focus:ring-blue-300 rounded-3xl bg-primary-500 px-6 py-3 text-center text-base font-normal text-white hover:bg-primary-600 focus:outline-none focus:ring-4"
-                onClick={handlePesanSekarang}
-              >
-                Pesan Sekarang
-              </button>
-            </div>
-          )}
+        <section className="flex h-full w-full flex-col justify-between gap-1 rounded-lg border-gray-200 bg-white p-5 md:rounded-l-lg">
+          <h5 className="mb-2 text-xl font-bold tracking-tight text-primary-950">
+            {packageDetail.packageName}
+          </h5>
+          <p className="mb-3 font-normal text-gray-700 ">
+            {packageDetail.description}
+          </p>
+          <p className="mb-3 font-normal text-primary-950">
+            <span className="font-bold">
+              {packageDetail.serviceDuration} Hari
+            </span>{' '}
+            Layanan
+          </p>
+          <section className="mb-4 flex flex-col items-center justify-between gap-2  md:flex-row">
+            <h4 className="text-xl font-bold text-primary-500">
+              {formatCurrency(packageDetail.price)},-
+            </h4>
+          </section>
         </section>
       </section>
-      {showOrderForm && <OrderForm />}
-    </section>
+      {packageDetail.destinations.length !== 0 &&
+        packageDetail.destinations.map((destination, index) => (
+          <DestinationDetails key={index} destinationData={destination} />
+        ))}
+      <div className="flex flex-row justify-end">
+        <Button variant={'primary'}>Edit</Button>
+      </div>
+    </div>
   )
 }
 
-export default PackageDetails
+export default PackagesDetail
